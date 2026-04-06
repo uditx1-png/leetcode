568885585c8d5fb96b14d96e83a98ced2d48bcb2
Udit Kumar <uditx1@gmail.com>
2026-04-06 23:36:19 +0530

    Add solution for Check if the Sentence Is Pangram

---
+++ easy/1832-Check-if-the-Sentence-Is-Pangram.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool checkIfPangram(string sentence) {
+        vector<bool>alpha(26,0); 
+
+        for(int i=0;i<sentence.size();i++){
+            alpha[sentence[i]-'a']=1;
+        };
+        for(int i=0;i<26;i++){
+            if(alpha[i]==0){
+                return false;
+            }
+        }
+        return true;
+    }
+int main(){
+    string sentence;
+    cout<<"enter the string = ";
+    cin>>sentence;
+    cout<<checkIfPangram(sentence)<<endl;
+    }
