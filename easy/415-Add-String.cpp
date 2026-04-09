fd3e48541b2733f211b6d7bb51972733d8f669ca
Udit Kumar <uditx1@gmail.com>
2026-04-09 20:37:47 +0530

    Add solution for 415 Add Strings

---
+++ easy/415-Add-String.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+string add(string num1, string num2){
+    int index1=num1.size()-1;
+    int index2=num2.size()-1;
+
+    int carry=0;
+    string ans;
+    int sum;
+
+    while(index2>=0){
+        sum=(num1[index1]-'0')+(num2[index2]-'0')+carry;
+        carry=sum/10;
+        char c='0'+sum%10;
+        ans+=c;
+        index1--;
+        index2--;
+    }
+
+    while(index1>=0){
+        sum=(num1[index1]-'0')+carry;
+        carry=sum/10;
+        char c='0'+sum%10;
+        ans+=c;
+        index1--;
+    }
+    if(carry){
+        ans+='1';
+    }
+    reverse(ans.begin(),ans.end()); 
+    return ans;
+
+}
+string addStrings(string num1,string num2){
+    if(num1.size()>num2.size()){
+        return add(num1,num2);
+    }
+    else{
+        return add(num2,num1);
+    }
+}
+int main(){
+    string num1,num2;
+    cout<<"enter the num1 = ";
+    cin>>num1;
+    cout<<"enter the num2 = ";
+    cin>>num2;
+
+    cout<<addStrings(num1, num2)<<endl;
+    return 0;
+}
